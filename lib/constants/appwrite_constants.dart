485f20e42390c469e5558503f132e0c3ce0257a8
dinodtharinda <dinodtharinda2001@gmail.com>
2023-05-08 20:08:26 +0530

    initial commitl

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String databaseId = '6458f3074b493a3071e7';
+  static const String projectId = '6458e6addf26714fa45b';
+  static const String endPoint = 'http://localhost:80/v1';
+}
+
